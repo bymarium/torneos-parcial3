36855cca55aa69df108cd4a2dd024794fe027b0a
Bymarium <bymarium@gmail.com>
2025-04-23 14:11:38 -0500

    crear metodo para guardar el torneo

--- Clases/clsTorneos.cs
+++ Clases/clsTorneos.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using torneos.Models;
 
 namespace torneos.Clases
 {
   public class clsTorneos
   {
+    private DBExamenEntities dbExamen = new DBExamenEntities();
+    public Torneo torneo { get; set; }
+
+    public string Guardar()
+    {
+      try
+      {
+        dbExamen.Torneos.Add(torneo);
+        dbExamen.SaveChanges();
+        return "¡Torneo guardado exitosamente";
+      }
+      catch (Exception ex)
+      {
+        return "Error al guardar el torneo: " + ex.Message;
+      }
+    }
   }
 }
