125abaeb2b140dabbc51c6d4247d602ddb0b5398
Bymarium <bymarium@gmail.com>
2025-04-23 14:35:45 -0500

    crear metodo post para guardar el torneo

--- Controllers/TorneosController.cs
+++ Controllers/TorneosController.cs
@@ -4,36 +4,21 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using torneos.Clases;
+using torneos.Models;
 
 namespace torneos.Controllers
 {
+  [RoutePrefix("api/torneos")]
   public class TorneosController : ApiController
   {
-    // GET api/<controller>
-    public IEnumerable<string> Get()
+    [HttpPost]
+    [Route("guardar")]
+    public string Insertar([FromBody] Torneo torneo)
     {
-      return new string[] { "value1", "value2" };
-    }
-
-    // GET api/<controller>/5
-    public string Get(int id)
-    {
-      return "value";
-    }
-
-    // POST api/<controller>
-    public void Post([FromBody] string value)
-    {
-    }
-
-    // PUT api/<controller>/5
-    public void Put(int id, [FromBody] string value)
-    {
-    }
-
-    // DELETE api/<controller>/5
-    public void Delete(int id)
-    {
+      clsTorneos torneos = new clsTorneos();
+      torneos.torneo = torneo;
+      return torneos.Guardar();
     }
   }
 }
