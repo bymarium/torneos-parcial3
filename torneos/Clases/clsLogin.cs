d6158a4e13eacc04bc9fc92a81217d464958fcbf
MANUELAG\manue <manuelagomezz1234chaparro@gmail.com>
2025-04-24 02:17:53 -0500

    Creacion de clases

---
+++ torneos/Clases/clsLogin.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using torneos.Models;
+using static torneos.Models.libLogin;
+
+namespace torneos.Clases
+{
+    public class clsLogin
+    {
+
+        private DBExamenEntities dbSuper = new DBExamenEntities();
+
+        public Login login { get; set; }
+
+        private LoginRespuesta logRpta;
+
+        private bool ValidarUsuario()
+
+        {
+
+            try
+
+            {
+
+                AdministradorITM admin = dbSuper.AdministradorITMs.FirstOrDefault(u => u.Usuario == login.Usuario);
+
+                if (admin == null)
+
+                {
+
+                    logRpta = new LoginRespuesta();
+
+                    logRpta.Mensaje = "Usuario no existe";
+
+                    return false;
+
+                }
+
+                login.Clave = admin.Clave;
+
+                return true;
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                logRpta = new LoginRespuesta();
+
+                logRpta.Mensaje = ex.Message;
+
+                return false;
+
+            }
+
+        }
+
+        public IQueryable<LoginRespuesta> Ingresar()
+
+        {
+
+            if (ValidarUsuario())
+
+            {
+
+                //string token = TokenGenerator.GenerateTokenJwt(login.Usuario);
+
+                string token = TokenGenerator.GenerateTokenJwt(login.Usuario);
+
+                return from U in dbSuper.Set<AdministradorITM>()
+
+                       where U.Usuario == login.Usuario &&
+
+                             U.Clave == login.Clave
+
+                       select new LoginRespuesta
+
+                       {
+
+                           Usuario = U.Usuario,
+
+                           Autenticado = true,
+
+                           Token = token,
+
+                           Mensaje = "Usuario autenticado",
+
+                       };
+
+            }
+
+            else
+
+            {
+
+                return null;
+
+            }
+
+        }
+
+
+    }
+}
---
+++ torneos/Models/libLogin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace torneos.Models
+{
+    public class libLogin
+    {
+        public class Login
+        {
+
+            public string Usuario { get; set; }
+            public string Clave { get; set; }
+
+        }
+
+        public class LoginRespuesta
+        {
+            public string Usuario { get; set; }
+            public bool Autenticado { get; set; }
+            public string Token { get; set; }
+            public string Mensaje { get; set; }
+
+        }
+    }
+}
